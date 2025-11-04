8aa4a67e172b1c07c5080d107ac64cb00cdbf8ac
prof-jblee <jungbini@sunmoon.ac.kr>
2025-11-04 23:27:59 +0900

    Separate files to sleep log and other sensor data for file size limitation error

--- app-service/time_service.js
+++ app-service/time_service.js
@@ -7,8 +7,10 @@ import { Step, Screen, HeartRate, Sleep } from "@zos/sensor";
 import { readFileSync, writeFileSync } from '@zos/fs'
 
 const moduleName = "Time Service"
-const SENSOR_FILE = 'sensor_data.json'        // /data 하위에 저장됨(앱별 샌드박스)
-const WRITE_QUEUE = []                        // 쓰기 대기 큐 (화면이 켜진 상태에서는 파일을 쓸 수 없기 때문)
+const SENSOR_FILE = 'sensor_data.json'        // 수면 로그를 제외한 센서 데이터 기록 파일 (걸음 수, 심박수)
+const SLEEP_FILE = 'sleep_data.json'          // 수면 로그 기록 파일
+const SENSOR_DATA_QUEUE = []                  // 센서데이터 대기 큐 (화면이 켜진 상태에서는 파일을 쓸 수 없기 때문)
+const SLEEP_LOG_QUEUE = []                    // 수면 로그 대기 큐
 let isFlushing = false
 
 const timeSensor = new Time()
@@ -17,15 +19,23 @@ const screen = new Screen()
 const heartrate = new HeartRate()
 const sleep = new Sleep()
 
+// 워치에 기록된 수면 로그의 변화를 관찰하기 위한 변수
+let prev_sleep_length = 0                     // 이전 순수 수면 시간 데이터
+let prev_total_sleep_length = 0               // 이전 총 수면 시간 데이터
+
+// 화면이 켜져 있는 상태에서 수면 로그를 파일로 기록하지 못해 notification을 보내면 안됨
+// 이때는 해당 변수를 true로 만들어 화면이 꺼질 때 notification을 보낼 수 있도록 함
+let isNotificationDelay = false               
+
 const hr_list = []                            // 분당 심박수를 누적하기 위한 리스트
 const step_list = []                          // 분당 걸음수를 누적하기 위한 리스트
 const light_list = []                         // 분당 조도값을 누적하기 위한 리스트
 
 const logger = log.getLogger("todo-list-page")
 
-// 화면이 꺼져있을 때만 WRITE_QUEUE에 있는 내용을 파일로 저장
-function flushStepQueue() {
-  if (isFlushing || WRITE_QUEUE.length === 0) return
+// 화면이 꺼져있을 때만 QUEUE에 있는 내용을 파일로 저장
+function flushSensorDataQueue() {
+  if (isFlushing || SENSOR_DATA_QUEUE.length === 0) return
   isFlushing = true
 
   try {
@@ -33,8 +43,8 @@ function flushStepQueue() {
     const arr = text ? JSON.parse(text) : []
 
     // 큐 데이터 병합
-    while (WRITE_QUEUE.length > 0) {
-      arr.push(WRITE_QUEUE.shift())
+    while (SENSOR_DATA_QUEUE.length > 0) {
+      arr.push(SENSOR_DATA_QUEUE.shift())
     }
 
     writeFileSync({
@@ -42,18 +52,59 @@ function flushStepQueue() {
       data: JSON.stringify(arr),
       options: { encoding: 'utf8' }
     })
-    console.log('[AppService] flushStepQueue: saved records =', arr.length)
+    console.log('[AppService] flushSensorDataQueue: saved records =', arr.length)
   } catch (e) {
-    console.log('[AppService] flushStepQueue error:', e)
+    console.log('[AppService] flushSensorDataQueue error:', e)
   } finally {
     isFlushing = false
   }
 }
 
-function appendSensorDataRecord() {
+// 화면이 꺼졌을 때 sleep log 큐에 있는 데이터를 파일로 기록
+// 이때, notification이 딜레이 된 경우라면 notification을 보냄
+function flushSleepLogQueue() {
+  if (isFlushing || SLEEP_LOG_QUEUE.length === 0) return
+  isFlushing = true
 
-  const ts = Date.now()    
-  const {score, startTime, endTime, totalTime} = sleep.getInfo()
+  try {
+    const text = readFileSync({ path: SLEEP_FILE, options: { encoding: 'utf8' } })
+    const arr = text ? JSON.parse(text) : []
+
+    // 큐 데이터 병합
+    while (SLEEP_LOG_QUEUE.length > 0) {
+      arr.push(SLEEP_LOG_QUEUE.shift())
+    }
+
+    writeFileSync({
+      path: SLEEP_FILE,
+      data: JSON.stringify(arr),
+      options: { encoding: 'utf8' }
+    })
+    console.log('[AppService] flushSleepLogQueue: saved records =', arr.length)
+
+    // Sleep log를 10분마다 체크할 때, 화면이 켜져있는 상태였다면 바로 notification을 보내지 않아 여기서 보냄
+    if(isNotificationDelay) {      
+      isNotificationDelay = false
+      notificationMgr.notify({
+        title: "수면 로그 변화 관찰",
+        content: `현재 새로운 수면 로그가 관찰되었습니다. +키를 눌러 서버에 데이터를 전송하세요.`,
+        actions: [
+          {
+            text: "Home Page",
+            file: "page/home/index.page",
+          },
+        ],
+      })
+    }
+
+  } catch (e) {
+    console.log('[AppService] flushSleepLogQueue error:', e)
+  } finally {
+    isFlushing = false
+  }
+}
+
+function appendSensorDataRecord(ts) {  
 
   // 1) 10분 동안의 누적 걸음 수 구하기
   let step_count = 0
@@ -84,28 +135,14 @@ function appendSensorDataRecord() {
 
   light_sum = light_list.reduce((acc, cur) => acc + cur, 0)
   light = light_sum / light_list.length  
-
-  // 4) awake 시간 구하기  
-  const sleepStage = sleep.getStage()
-  let awake_length = 0
-  sleepStage.forEach((sleep_info) => {
-    const { model, start, stop } = sleep_info
-
-    if (model === sleep.getStageConstantObj().WAKE_STAGE) {
-      awake_length += (Number(stop) - Number(start))
-    }
-  })
-
-  const sleepLength = totalTime - awake_length    // 실제 수면 시간 구하기
   
-  // 2) 새 레코드 추가
-  const record = { ts, step_count, light, heart_rate, score, startTime, endTime, sleepLength, totalTime }
-
-  // 3) 화면이 켜져 있으면 즉시 쓰기 금지 → 큐에 저장
-  if (screen.getLight() > 0) {    
-    console.log('Screen on is Checked')
-    WRITE_QUEUE.push(record)
-    console.log('[AppService] screen ON → queued record. queue size=', WRITE_QUEUE.length)
+  // 4) 새 레코드 추가
+  const sensor_record = { ts, step_count, light, heart_rate }
+    
+  // 5) 화면이 켜져 있으면 즉시 쓰기 금지 → 큐에 저장
+  if (screen.getLight() > 0) {        
+    SENSOR_DATA_QUEUE.push(sensor_record)    
+    console.log('[AppService] screen ON → queued record. queue size=', SENSOR_DATA_QUEUE.length)
 
     // QUEUE에 들어갔으므로 각 리스트 비우기
     hr_list.length = 0    
@@ -115,18 +152,18 @@ function appendSensorDataRecord() {
     return
   }
 
-  // 4) 화면 꺼져 있음 → 즉시 flush
+  // 6) 화면 꺼져 있음 → 즉시 flush
   try {
     const text = readFileSync({ path: SENSOR_FILE, options: { encoding: 'utf8' } })
     const arr = text ? JSON.parse(text) : []
-    arr.push(record)
+    arr.push(sensor_record)
 
     writeFileSync({
       path: SENSOR_FILE,
       data: JSON.stringify(arr),
       options: { encoding: 'utf8' }
     })
-    console.log('[AppService] saved:', JSON.stringify(record))
+    console.log('[AppService] saved:', JSON.stringify(sensor_record))
 
     // 파일에 기록 했으므로 각 리스트 비우기
     hr_list.length = 0    
@@ -135,6 +172,69 @@ function appendSensorDataRecord() {
 
   } catch (e) {
     console.log('[AppService] write error:', e)
+  }
+}
+
+function appendSleepLogRecord(ts) {
+   
+  const {score, startTime, endTime, totalTime} = sleep.getInfo()    // 수면 로그 데이터 가져오기
+
+  // 수면 중 깬 시간 구하기  
+  const sleepStage = sleep.getStage()
+  let awake_length = 0
+  sleepStage.forEach((sleep_info) => {
+    const { model, start, stop } = sleep_info
+
+    if (model === sleep.getStageConstantObj().WAKE_STAGE) {
+      awake_length += (Number(stop) - Number(start))
+    }
+  })
+  
+  const sleepLength = totalTime - awake_length                       // 실제 수면 시간 구하기
+
+  // 최초 기록된 실제 수면 시간 또는 총 수면 시간과 다르다면 prev_xxx 변수에 다시 기록하고 notification으로 알림
+  if (prev_sleep_length !== sleepLength || prev_total_sleep_length !== totalTime) {
+    prev_sleep_length = sleepLength
+    prev_total_sleep_length = totalTime
+
+    const sleep_record = { ts, score, startTime, endTime, sleepLength, totalTime }    
+
+    // 화면이 켜져 있으면 즉시 쓰기 금지 → 큐에 저장
+    if (screen.getLight() > 0) {        
+      SLEEP_LOG_QUEUE.push(sleep_record)    
+      isNotificationDelay = true;           // notification을 화면이 꺼졌을 때로 미룸
+      console.log('[AppService] screen ON → queued record. queue size=', SLEEP_LOG_QUEUE.length)
+      return
+    }
+
+    // 화면 꺼져 있음 → 즉시 flush
+    try {
+      const text = readFileSync({ path: SLEEP_FILE, options: { encoding: 'utf8' } })
+      const arr = text ? JSON.parse(text) : []
+      arr.push(sleep_record)
+
+      writeFileSync({
+        path: SLEEP_FILE,
+        data: JSON.stringify(arr),
+        options: { encoding: 'utf8' }
+      })
+      console.log('[AppService] saved:', JSON.stringify(sleep_record))
+   
+    } catch (e) {
+      console.log('[AppService] write error:', e)
+    }
+    
+    // 사용자 폰으로 알람 주기
+    notificationMgr.notify({
+      title: "수면 로그 변화 관찰",
+      content: `현재 새로운 수면 로그가 관찰되었습니다. +키를 눌러 서버에 데이터를 전송하세요.`,
+      actions: [
+        {
+          text: "Home Page",
+          file: "page/home/index.page",
+        },
+      ],
+    })
   }
 }
 
@@ -170,17 +270,36 @@ AppService({
   onInit(e) {
     logger.log(`service onInit(${e})`)
 
-    // 심박수는 onCurrentChange 함수를 호출해야 내부적으로 기록됨
-    heartrate.onCurrentChange(() => { })
-
-    // 화면 꺼질 때 flush
+    // 화면 꺼질 때 flush 하는 콜백 함수 등록
     screen.onChange(() => {
       console.log("Screen Status: ", screen.getStatus())
       if (screen.getStatus() === 2) {
         console.log('[AppService] screen OFF → flush queue')
-        flushStepQueue()
+        flushSensorDataQueue()          // 센서 데이터 큐에 있는 내용을 파일로 기록
+        flushSleepLogQueue()            // 슬립 로그 큐에 있는 내용을 파일로 기록
       }
     })
+
+    // 심박수는 onCurrentChange 함수를 호출해야 내부적으로 기록됨
+    heartrate.onCurrentChange(() => { })
+
+    // 현재 워치에 기록된 실제 수면 시간, 총 수면시간 기록
+    const {totalTime} = sleep.getInfo()     // 총 수면 시간
+    
+    // 수면 중 깬 시간 구하기  
+    const sleepStage = sleep.getStage()
+    let awake_length = 0
+    sleepStage.forEach((sleep_info) => {
+      const { model, start, stop } = sleep_info
+
+      if (model === sleep.getStageConstantObj().WAKE_STAGE) {
+        awake_length += (Number(stop) - Number(start))
+      }
+    })
+
+    prev_total_sleep_length = totalTime             // 총 수면 시간
+    prev_sleep_length = totalTime - awake_length    // 실제 수면 시간
+
 
     // 1분 간격 반복
     timeSensor.onPerMinute(() => {
@@ -200,12 +319,15 @@ AppService({
       // 10분 간격으로 실행하기 위한 코드
       const h = timeSensor.getHours()
       const m = timeSensor.getMinutes()
+
+      const ts = Date.now()       // 현재 시각 저장
       
       if (m % 10 === 0) {
         logger.log(
           `${moduleName} time report: ${timeSensor.getHours()}:${timeSensor.getMinutes()}:${timeSensor.getSeconds()}`
         ),
-        appendSensorDataRecord()
+        appendSensorDataRecord(ts)        // 센서 데이터를 10분 간격으로 파일로 기록        
+        appendSleepLogRecord(ts)          // 수면 로그를 10분 간격으로 관찰하여 변화가 감지되면 기록 후, 알람
       }      
     })
 
